e235767dbce78982749aed2c3a7def808cc70551
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-13 11:59:50 -0600

    added project files. Completed persona in index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+		<link rel="stylesheet" href="./style.css">
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<hr>
+		<div id="personaContainer">
+			<h2>Persona</h2>
+			<h3>Jack Jackson</h3>
+			<ul id="persona">
+				<li><strong>Age:</strong> 32</li>
+				<li><strong>Gender:</strong> Male</li>
+				<li><strong>Profession:</strong> Web Developer (Front End)</li>
+				<li><strong>Technology:</strong> 2017 Macbook Pro, macOS 10.13 High Sierra</li>
+				<li><strong>Attitude:</strong> Jack uses Github to share his code with his peers. Github is nice, but there are several requisite steps before a user can preview how code is displayed by the browser. Jack needs a simpler solution.</li>
+				<li><strong>Goals:</strong> Jack needs a simple method for sharing his HTML/CSS/JS projects with the world. He should be able to save his code to the app, or better yet, use the app to code while a live preview shows him the changes he is making. Others should be able to see his code and how it's displayed by the browser all in one place without the need for intermediate steps.</li>
+				<li><strong>Frustrations:</strong> Jack feels that his front-end skills are not appreciated by his peers. He's created several exciting visual projects and wishes to share them, but the disconnect between a git repo and the browser representation of his code means that very few people see his work.</li>
+			</ul>
+		</div>
+		<hr>
+		<div id="userStoryContainer">
+			<h2>User Story</h2>
+		</div>
+	</body>
+</html>
