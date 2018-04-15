f1a025f652dc4c713f3cbfb7744624ee2f6159e2
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-15 10:14:58 -0600

    added partial conceptual model

--- index.php
+++ index.php
@@ -16,13 +16,14 @@
 				<li><strong>Gender:</strong> Male</li>
 				<li><strong>Profession:</strong> Web Developer (Front End)</li>
 				<li><strong>Technology:</strong> 2017 Macbook Pro, macOS 10.13 High Sierra</li>
-				<li><strong>Attitude:</strong> Jack uses Github to share his code with his peers. Github is nice, but there are several requisite steps before a user can preview how code is displayed by the browser. Jack needs a simpler solution.</li>
+				<li><strong>Attitude:</strong> Jack uses Github to share his code with his peers. Github is nice, but there are several additional steps before a user can preview how code is displayed by the browser. Jack needs a simpler solution.</li>
 				<li><strong>Goals:</strong> Jack needs a simple method for sharing his HTML/CSS/JS projects with the world. He should be able to save his code to the app, or better yet, use the app to code while a live preview shows him the changes he is making. Others should be able to see his code and how it's displayed by the browser all in one place without the need for intermediate steps.</li>
 				<li><strong>Frustrations:</strong> Jack feels that his front-end skills are not appreciated by his peers. He's created several exciting visual projects and wishes to share them, but the disconnect between a git repo and the browser representation of his code means that very few people see his work.</li>
 			</ul>
 		</div>
 		<hr>
 		<div id="userStoriesContainer">
+			<h2>User Stories</h2>
 			<p>As a registered user, Jack wants to post his HTML/CSS/JS code for others to see, in both raw and rendered format.</p>
 			<p>As a public user, I want to browse others' code and see it represented visually by the app.</p>
 			<p>As a registered user, I want to comment on a post.</p>
@@ -44,6 +45,43 @@
 			</ol>
 		</div>
 		<hr>
-		<div id="conceptualModel"></div>
+		<div id="conceptualModel">
+			<h2>Conceptual Model</h2>
+			<ul>
+				<li><strong>profile</strong>
+					<ul>
+						<li>profileId (primary key)</li>
+						<li>profileName</li>
+						<li>profileEmail</li>
+						<li>profileAvatar</li>
+					</ul>
+				</li>
+				<li><strong>pen</strong>
+					<ul>
+						<li>penProfileId</li>
+					</ul>
+				</li>
+				<li><strong>penHtml</strong>
+					<ul>
+
+					</ul>
+				</li>
+				<li><strong>penCss</strong>
+					<ul>
+
+					</ul>
+				</li>
+				<li><strong>penJs</strong>
+					<ul>
+
+					</ul>
+				</li>
+				<li><strong>penComments</strong>
+					<ul>
+
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</body>
 </html>
