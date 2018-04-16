c8bf95d15812c0ef2b0114ba0426bc106418f745
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-16 15:42:59 -0600

    cleaned up SQL file (alphabetized/reordered entries)

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -7,14 +7,14 @@ DROP TABLE IF EXISTS profile;
 CREATE TABLE profile (
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
-	profileName VARCHAR(32) NOT NULL,
-	profileUsername VARCHAR(32) NOT NULL,
-	profileEmail VARCHAR(128) NOT NULL,
 	-- Avatars stored in file system, DB stores the FILEPATH
 	-- VARCHAR VALUE ???
 	profileAvatar VARCHAR(MAX),
+	profileEmail VARCHAR(128) NOT NULL,
 	-- profileIsPro acts as a bool to check Pro account status
 	profileIsPro TINYINT NOT NULL,
+	profileName VARCHAR(32) NOT NULL,
+	profileUsername VARCHAR(32) NOT NULL,
 	UNIQUE(profileId),
 	UNIQUE(profileEmail),
 	PRIMARY KEY(profileId)
@@ -23,11 +23,11 @@ CREATE TABLE profile (
 CREATE TABLE pen (
 	penId BINARY(16) NOT NULL,
 	penProfileId BINARY(16) NOT NULL,
-	penName VARCHAR(64),
 	-- pen code content attrs work like profileAvatar
 	penHtmlContent VARCHAR(MAX),
 	penCssContent VARCHAR(MAX),
 	penJsContent VARCHAR(MAX),
+	penName VARCHAR(64),
 	UNIQUE(penId),
 	INDEX(penProfileId),
 	FOREIGN KEY(penProfileId) REFERENCES profile(profileId),
@@ -37,10 +37,10 @@ CREATE TABLE pen (
 CREATE TABLE comment (
 	commentProfileId BINARY(16) NOT NULL,
 	commentPenId BINARY(16) NOT NULL,
--- 	commentId BINARY(16) NOT NULL, <-not needed ???
-	commentDateTime DATETIME(6),
 	-- commentContent also stores filepath
 	commentContent VARCHAR(MAX),
+	commentDateTime DATETIME(6),
+	-- commentId BINARY(16) NOT NULL, <-not needed ???
 	INDEX(commentProfileId),
 	INDEX(commentPenId),
 	FOREIGN KEY(commentProfileId) REFERENCES profile(profileId),
