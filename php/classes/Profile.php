a3de5a586a850666b841fc1aa0a3ef67a6f68979
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-19 14:24:28 -0600

    added mutator methods for Profile (id, activationToken, avatar, email)

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,7 +1,6 @@
 <?php
 
 class Profile implements \JsonSerializable {
-	use ValidateDate;
 	use ValidateUuid;
 	/**
 	 * id for this Profile; this is the primary key
@@ -58,7 +57,7 @@ public function getProfileId() : Uuid {
  * @throws \RangeException if $newProfileId is not positive
  * @throws \TypeError if $newProfileId is not a uuid or string
  **/
-public function setProfileId( $newProfileId) : void {
+public function setProfileId($newProfileId) : void {
 	try {
 		$uuid = self::validateUuid($newProfileId);
 	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -80,12 +79,48 @@ public function getProfileActivationToken() : string {
 }
 
 /**
+ * mutator method for profileActivationToken
+ *
+ * @param string $newProfileActivationToken new value of profileActivationToken
+ * @throws \TypeError if $newProfileActivationToken is not a string
+ * @throws \RangeException if $newProfileActivationToken > 32chars OR empty string
+ **/
+public function setProfileActivationToken($newProfileActivationToken) : void {
+	if(gettype($newProfileActivationToken) != string) {
+		throw(new \TypeError("Input is not a string"));
+	}
+
+	if(0 > strlen($newProfileActivationToken) > 32 || empty($newProfileActivationToken) === true) {
+		throw(new \RangeException("Activation token empty or too large"));
+	}
+
+}
+
+/**
  * accessor method for profileAvatar
  *
  * @return string value of filepath to user's avatar
  **/
 public function getProfileAvatar() : string {
 	return($this->profileAvatar);
+}
+
+/**
+ * mutator method for profileAvatar
+ *
+ * @param string $newProfileAvatar new value of profileAvatar
+ * @throws \TypeError if input is not a string
+ * @throws \RangeException if 0chars > $newProfileAvatar > 512chars
+ **/
+public function setProfileAvatar($newProfileAvatar) : void {
+	if(gettype($newProfileAvatar) != string) {
+		throw(new \TypeError("Input is not a string"));
+	}
+
+	if(0 > strlen($newProfileAvatar) > 512) {
+		throw(new \RangeException("Avatar filepath input too large"));
+	}
+
 }
 
 /**
@@ -98,10 +133,35 @@ public function getProfileEmail() : string {
 }
 
 /**
+ * mutator method for profileEmail
+ *
+ * @param string $newProfileEmail new val for profileEmail
+ * @throws \InvalidArgumentException if input is insecure
+ * @throws \TypeError if $newProfileEmail is not a string
+ * @throws \RangeException if $newProfileEmail > 128chars
+ **/
+public function setProfileEmail($newProfileEmail) : void {
+	// verify the token is secure
+	$newProfileEmail = trim($newProfileEmail);
+	$newProfileEmail = filter_var($newProfileEmail, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(empty($newProfileEmail) === true) {
+		throw(new \InvalidArgumentException("Email value is empty or insecure"));
+	}
+
+	if(gettype($newProfileEmail) != string) {
+		throw(new \TypeError("Email input is not a string"));
+	}
+
+	if(strlen($newProfileEmail) > 128) {
+		throw(new \RangeException("Email input too large"))
+	}
+}
+
+/**
  * accessor method for profileIsPro
  *
  * @return bool value of profileIsPro
- */
+ **/
 public function getProfileIsPro() : bool {
 	return($this->profileIsPro);
 }
@@ -110,7 +170,7 @@ public function getProfileIsPro() : bool {
  * accessor method for profileName
  *
  * @return string value of profileName
- */
+ **/
 public function getProfileName() : string {
 	return($this->profileName);
 }
@@ -119,7 +179,7 @@ public function getProfileName() : string {
  * accessor method for profileUsername
  *
  * @return string value of profileUsername
- */
+ **/
 public function getProfileUsername() : string {
 	return($this->profileUsername);
 }
