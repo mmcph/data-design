80ef270ea0e660fe92e7f8f69419a12a0e0e79b5
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-23 22:16:55 -0600

    added insert/delete/update for Pen class

--- sql/oml-assignment.sql
+++ sql/oml-assignment.sql
@@ -61,15 +61,15 @@
 	public function insert(\PDO $pdo) : void {
 
 		-- create query template
-		$query = "INSERT INTO pen(profileId, profileActivationToken, profileAvatar, profileEmail, profileIsPro, profileName, profileUsername) VALUES(:profileId, :profileActivationToken, :profileAvatar, :profileEmail, :profileIsPro, :profileName, :profileUsername)";
+		$query = "INSERT INTO pen(penId, penProfileId, penName, penHtmlContent, penCssContent, penJsContent) VALUES(:penId, :penProfileId, :penName, :penHtmlContent, :penCssContent, :penJsContent)";
 		$statement = $pdo->prepare($query);
 
 		-- bind the member variables to the placeholders in the template
-		$parameters = ["profileId" => $this->profileID->getBytes(), "profileActivationToken" => $this->profileActivationToken, "profileAvatar" => $this->profileAvatar, "profileEmail" => $this->profileEmail, "profileIsPro" => $this->profileIsPro, "profileName" => $this->profileName, "profileUsername" => $this->profileUsername];
+		$parameters = ["penProfileId" => $this->penProfileId->getBytes(), "penId" => $this->penId->getBytes(), "penName" => $this->penName, "penHtmlContent" => $this->penHtmlContent, "penCssContent" => $this->penCssContent, "penJsContent" => $this->penJsContent];
 		$statement->execute($parameters);
 	}
 
--- deletes this profile from mysql
+-- deletes this pen from mysql
 --
 -- @param \PDO $pdo PDO connection object
 -- @throws \PDOException when mySQL related errors occur
@@ -78,15 +78,15 @@
 	public function delete(\PDO $pdo) : void {
 
 		-- create query template
-		$query = "DELETE FROM profile WHERE profileId = :profileId";
+		$query = "DELETE FROM pen WHERE penId = :penId";
 		$statement = $pdo->prepare($query);
 
 		-- bind member vars to placeholder in template
-		$parameters = ["profileId" => $this->profileId->getBytes()];
+		$parameters = ["penId" => $this->penId->getBytes()];
 		$statement->execute($parameters);
 	}
 
--- updates this profile in mysql
+-- updates this pen in mysql
 --
 -- @param \PDO $pdo PDO connection object
 -- @throws \PDOException when mySQL related errors occur
@@ -95,9 +95,9 @@
 	public function update(\PDO $pdo) : void {
 
 		-- create query template
-		$query = "UPDATE profile SET profileActivationToken = :profileActivationToken, profileAvatar = :profileAvatar, profileEmail = :profileEmail, profileIsPro = :profileIsPro, profileName = :profileName, profileUsername = :profileUsername WHERE profileId = :profileId";
+		$query = "UPDATE pen SET penName = :penName, penHtmlContent = :penHtmlContent, penCssContent = :penCssContent, penJsContent = :penJsContent WHERE penId = :penId";
 		$statement = $pdo->prepare($query);
 
-		$parameters = ["profileId" => $this->profileId->getBytes(), "profileActivationToken" => $this->profileActivationToken, "profileAvatar" => $this->profileAvatar, "profileEmail" => $this->profileEmail, "profileIsPro" => $this->profileIsPro, "profileName" => $this->profileName, "profileUsername" => $this->profileUsername];
+		$parameters = ["penId" => $this->penId->getBytes(), "penName" => $this->penName, "penHtmlContent" => $this->penHtmlContent, "penCssContent" => $this->penCssContent, "penJsContent" => $this->penJsContent];
 		$statement->execute($parameters);
 	}
